ff2df405e34e2c47f4678368cc671d94d0278d2e
aymensellaouti <aymen.sellaouti@gmail.com>
2025-11-17 15:58:15 +0100

    mini Word correction

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-mini-word',
-  templateUrl: './mini-word.component.html',
-  styleUrls: ['./mini-word.component.css']
+  selector: "app-mini-word",
+  templateUrl: "./mini-word.component.html",
+  styleUrls: ["./mini-word.component.css"],
 })
 export class MiniWordComponent {
-
+  color = "black";
+  fontFamily = "garamond";
+  fontSize = 75;
 }
