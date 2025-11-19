ca2eb0612b6d1d919debf011ce3e41126aa163d8
aymensellaouti <aymen.sellaouti@gmail.com>
2025-11-19 14:07:07 +0100

    correction slider

--- src/app/rxjs/slider/slider.component.ts
+++ src/app/rxjs/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, timer, map } from "rxjs";
 
 @Component({
   selector: "app-slider",
@@ -18,5 +18,9 @@ export class SliderComponent {
     "rotating_card_profile3.png",
   ];
 
-  paths$!: Observable<string>;
+  paths$: Observable<any> = timer(0, this.timer).pipe(
+    // 0 1 2 3 4 5 6 7 8 9 10
+    map((index) => this.imagesArray[index % this.imagesArray.length])
+    // paths[0] paths[1]
+  );
 }
