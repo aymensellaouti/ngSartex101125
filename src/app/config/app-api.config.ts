b927feb2625d3e68a5d1ccca6971ca827861020d
aymensellaouti <aymen.sellaouti@gmail.com>
2025-11-20 10:46:53 +0100

    add api login

--- src/app/config/app-api.config.ts
+++ src/app/config/app-api.config.ts
@@ -2,4 +2,5 @@ export const APP_API = {
   todo: "https://jsonplaceholder.typicode.com/todos",
   cvSwagger: "https://apilb.tridevs.net/explorer/#!/personne/personne_find",
   cvs: "https://apilb.tridevs.net/api/personnes/",
+  login: "https://apilb.tridevs.net/api/Users/login/",
 };
