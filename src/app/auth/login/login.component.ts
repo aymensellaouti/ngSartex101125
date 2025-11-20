e610aea8c84e0e798374ec70a548a142e51f5a57
aymensellaouti <aymen.sellaouti@gmail.com>
2025-11-20 10:53:08 +0100

    listes des taches

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
-
+  // Todo ajouter la méthode login
+  // Todo Créer le service auth avec une méthode login
+  // Todo Créer le DTO de la réponse de login
+  // Todo Créer le DTO de les credentials
+  // Todo modifier le delete en récupérant le token et en
+  // l'envoyant dans les headers ou dans les params
 }
